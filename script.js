aaf9d306a2bb6c05a00d44b151bea415ddfab691
WhyNotjjjj <110331202+WhyNotjjjj@users.noreply.github.com>
2022-08-20 16:49:16 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+  $(document).ready(function() {
+    $('.burger').click(function() {
+        $('.burger, header nav').toggleClass('active');
+        $('nav a').click(function() {
+            $('.burger, header nav').removeClass('active');
+        });
+    });        
+});
+
+$(document).ready(function () {
+    $('.slider').bxSlider({
+        pagerCustom: '.slider-nav',
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+        speed: 1200,
+        mode: 'horizontal',
+    });
+});
